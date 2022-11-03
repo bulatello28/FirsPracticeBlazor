aa765563c0a9c9dd982e9b022ca6e743773bcba9
bulatello28 <bulatalmazovich.99@mail.ru>
2022-11-03 10:54:59 +0300

    Добавьте файлы проекта.

---
+++ FirsPracticeBlazor/Data/User.cs
@@ -0,0 +1,17 @@
+﻿namespace FirsPracticeBlazor.Data
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public User(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+       
+    }
+}
---
+++ FirsPracticeBlazor/Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using FirsPracticeBlazor.Data;
+
+namespace FirsPracticeBlazor.Services
+{
+    public  class UserService
+    {
+        private static readonly List<User> Users = new List<User>()
+        {
+            new User("Bulat", "Shaykhutdinov", 18),
+            new User("Roman", "Vasil'ev", 17),
+            new User("Riyaz", "Zaripov", 18),
+            new User("Shamil", "Sharipov", 18)
+        };
+
+        public Task<User[]> GetUsers()
+        {
+            var users = new List<User>();
+            for (int i = 0; i < Random.Shared.Next(1, 10); ++i)
+            {
+                users.Add(Users[Random.Shared.Next(0, Users.Count - 1)]);
+            }
+
+            return Task.FromResult(users.ToArray());
+        }
+
+    }
+}
